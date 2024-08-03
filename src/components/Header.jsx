72b5aea1f0883d122bc296d6c9fda88a3ce8f1e6
PC <PC@DESKTOP-2VPR9IB>
2024-08-03 19:57:22 -0300

    site em construção

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <h1 className="text-danger">Em construção!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
